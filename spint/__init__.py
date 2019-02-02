d19edfc1cbacf55b8a5ce3b4d710f1923070791f
tamos <tyler.amos@alumni.carleton.ca>
2019-02-01 18:24:03 -0600

    updated init

--- spint/__init__.py
+++ spint/__init__.py
@@ -1,6 +1,7 @@
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 from .gravity import Gravity, Production, Attraction, Doubly
 from .utils import CPC, sorensen, srmse
 from .vec_SA import VecMoran as Moran_Vector
 from .dispersion import phi_disp, alpha_disp
+from .radiation import Radiation
